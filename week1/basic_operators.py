290a071dfa6ba4f728a03a27232425b9095f7849
Milasch23 <kamyxsch@gmail.com>
2026-02-03 16:30:17 -0300

    Add: basic operators

---
+++ week1/basic_operators.py
@@ -0,0 +1,39 @@
+# OPERATORS
+
+# Arithmetic operators
+
+addition = 10 + 4
+substraction = 100 - 50
+multiplication = 10 * 10
+division = 25 / 5
+floor_div = 10 // 4
+modulus = 7 % 3
+exponentiation = 5 ** 5
+
+# Assigment operators
+a = 5
+a += 7  # a = a + 7
+a -= 1  # a = a - 1
+a *= 3
+a /= 2
+a //= 2
+a %= 3
+a **= 5
+
+# Comparison operators: return a boolean result
+equal_to = 5 == 5
+notEqualTo = 5 != 7
+greater_than = 6 > 10
+less_than = 6 < 8
+greaterEqual = 10 >= 10
+less_equal = 10 <= 5
+
+# Logical operators
+# And: both statements must be True, otherwise it will return False
+aNd = (6 > 8) and (5 < 15)
+
+# Or: only one statement has to be True. If both are false, the result will be False
+oR = (5 > 10) or (7 <= 7)
+
+# Not: flips the result of the statement
+nOt = not (45 == 45)
