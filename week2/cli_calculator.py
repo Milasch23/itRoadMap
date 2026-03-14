3795a8e15bb99acafc74fe87cab6be16d74d45c1
Milasch23 <kamyxsch@gmail.com>
2026-03-14 15:35:43 -0300

    First comments

--- week2/cli_calculator.py
+++ week2/cli_calculator.py
@@ -1,3 +1,4 @@
+# We define the operation functions
 def addition(a, b):
     return a + b
 
@@ -7,22 +8,25 @@ def subtraction(a, b):
 def multiplication(a, b):
     return a * b
 
-def division(a, b):
+def division(a, b): #This one is different, so we can avoid a zero division
     if b == 0:
-        raise ValueError("Error: zero division is not allowed.")
+        raise ValueError("Error: zero division is not allowed.") 
     return a / b
     
+# Menu function
 def show_menu():
     print("\n" + "*-"*35)
     print("CLI Calculator")
     print("="*35)
-    print("""1. Addition
+    print("""
+          1. Addition
           2. Subtraction
           3. Multiplication
           4. Division
           0. Exit""")
     print("="*35)
 
+# We define a function for the choice. It accepts a message as a parameter.
 def ask_num(message):
     while True:
         try:
@@ -30,25 +34,30 @@ def ask_num(message):
         except ValueError:
             print("Please, enter a valid number")
 
+# Main function
 def main():
     print("Welcome to my CLI Calculator!")
 
     while True:
-        show_menu()
-        option = input("Choose the math operation: ").strip()
+        show_menu() #We first call the menu function
+        option = input("Choose the math operation: ").strip() #We use the strip function to remove spaces
 
+        # If statement that ends the program if the user types 0
         if option == "0":
             print("See you later!")
             break
 
+        # If statement that validates the choice
         if option not in ["1", "2", "3", "4"]:
             print("Invalid option. Try again.")
             continue
-
+        
+        # We call the choice function, and save the results in two different variables
         a = ask_num("Type the first number: ")
         b = ask_num("Type th second number: ")
 
         try:
+            # if - elif statement for operation function calls
             if option == "1":
                 result = addition(a, b)
                 symbol = "+"
@@ -70,5 +79,6 @@ def main():
         except ValueError as e:
             print(f"\n {e}")
 
+# Finally, we call the main function
 if __name__ == "__main__":
     main()
