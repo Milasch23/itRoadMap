c4840b3f7f2f763f95f61ba50ba6e6afe21c0e91
Milasch23 <kamyxsch@gmail.com>
2026-03-14 14:53:08 -0300

    Add: CLI Calculator mini project

---
+++ week2/cli_calculator.py
@@ -0,0 +1,74 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Error: zero division is not allowed.")
+    return a / b
+    
+def show_menu():
+    print("\n" + "*-"*35)
+    print("CLI Calculator")
+    print("="*35)
+    print("""1. Addition
+          2. Subtraction
+          3. Multiplication
+          4. Division
+          0. Exit""")
+    print("="*35)
+
+def ask_num(message):
+    while True:
+        try:
+            return float(input(message))
+        except ValueError:
+            print("Please, enter a valid number")
+
+def main():
+    print("Welcome to my CLI Calculator!")
+
+    while True:
+        show_menu()
+        option = input("Choose the math operation: ").strip()
+
+        if option == "0":
+            print("See you later!")
+            break
+
+        if option not in ["1", "2", "3", "4"]:
+            print("Invalid option. Try again.")
+            continue
+
+        a = ask_num("Type the first number: ")
+        b = ask_num("Type th second number: ")
+
+        try:
+            if option == "1":
+                result = addition(a, b)
+                symbol = "+"
+
+            elif option == "2":
+                result = subtraction(a, b)
+                symbol = "-"
+
+            elif option == "3":
+                result = multiplication(a, b)
+                symbol = "*"
+
+            elif option == "4":
+                result = division(a, b)
+                symbol = "/"
+
+            print(f"\n {a} {symbol} {b} = {result}")
+        
+        except ValueError as e:
+            print(f"\n {e}")
+
+if __name__ == "__main__":
+    main()
